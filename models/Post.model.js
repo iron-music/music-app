9fcef7bb0f1520718753a2bca8d4d90249b97383
Martin Palkov <martin.palkov@gmail.com>
2023-11-13 15:38:02 +0100

    User and Post models v.01

---
+++ models/Post.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+const postSchema = new Schema(
+    {
+        title: String,
+        artist: String,
+        songSpotiURI: String,
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        postText: String
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true
+    }
+);
+
+const Post = model("Post", postSchema);
+
+module.esports = Post;
