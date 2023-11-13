1131bd774b8e69c19cc8ecd6b0d0bfe5d3ad83c1
Marc.A <marcalberopascual@gmail.com>
2023-11-13 16:14:36 +0100

    passing selected song to create-post

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -18,6 +18,10 @@ res.render("users/profile",{userName});
 router.get("/:user/create",  (req, res)=>{
   const userName = req.params.user;
   const song = req.query.song;
+  const info = {
+    userName:userName
+   
+  }
   if (song) {
 
     spotifyApi.searchTracks(song)
@@ -29,18 +33,28 @@ router.get("/:user/create",  (req, res)=>{
       const cleanSongsArr = songsArray.map((song)=>{
           const obj = {
             name: song.name,
+            artist: song.artists[0].name,
             id: song.id,
             preview: song.preview_url
           }
-          return obj
+          return {... obj,
+            stringified: JSON.stringify(obj)}
       })
-      res.render("users/create-post", {userName, cleanSongsArr});
-      //res.send(cleanSongsArr)
+      info.cleanSongsArr = cleanSongsArr;
+      res.render("users/create-post", info);
     })
   }
   else{
-      res.render("users/create-post", {userName});
+      res.render("users/create-post", info);
     }
   })
+  
+router.post("/:user/create", (req, res) =>{
+  console.log(req.body.selectedSong)
+  res.send(req.body.selectedSong)
+
+
+
+})
 
 module.exports = router;
