3b0f22f2fd60abaa683ac37e1351afcc9e0b2b86
Marc.A <marcalberopascual@gmail.com>
2023-11-13 12:12:50 +0100

    login render  modified, profile view and route created

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const isLoggedIn = require('../middleware/isLoggedIn');
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/:user", isLoggedIn, async( req, res)=>{
+const userName = req.params.user;
+render("users/profile",{userName});
 });
 
 module.exports = router;
