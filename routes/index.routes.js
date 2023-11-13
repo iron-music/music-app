277cbd4ba7e5736e68f0f5b847833fb4b5a3235a
Marc.A <marcalberopascual@gmail.com>
2023-11-13 15:02:12 +0100

    search song displaying results

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const spotifyApi = require("../api/api");
+
 
 const router = express.Router();
 
@@ -11,5 +13,34 @@ router.get("/:user", async( req, res)=>{
 const userName = req.params.user;
 res.render("users/profile",{userName});
 });
+
+
+router.get("/:user/create",  (req, res)=>{
+  const userName = req.params.user;
+  const song = req.query.song;
+  if (song) {
+
+    spotifyApi.searchTracks(song)
+      .then((result)=>{
+      
+        return result.body.tracks.items
+    })
+    .then((songsArray)=>{
+      const cleanSongsArr = songsArray.map((song)=>{
+          const obj = {
+            name: song.name,
+            id: song.id,
+            preview: song.preview_url
+          }
+          return obj
+      })
+      res.render("users/create-post", {userName, cleanSongsArr});
+      //res.send(cleanSongsArr)
+    })
+  }
+  else{
+      res.render("users/create-post", {userName});
+    }
+  })
 
 module.exports = router;
