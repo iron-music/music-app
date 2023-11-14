117c7806793d025459ccd3acc87e84998e260140
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-11-14 12:19:43 +0100

    update posts done

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -8,15 +8,17 @@ const Post = require("../models/Post.model");
 
 //render view
 router.get("/:post/edit", async (req, res) =>{
-    console.log("requparams",req.params);
+    // console.log("requparams",req.params);
     selectedPost = await Post.findById(req.params.post)
     res.render("post/editing-post", selectedPost);
-    console.log("post",selectedPost);
+    // console.log("post",selectedPost);
 })
 
 //redirect
-// router.put("/:post/edit", async (req, res) =>{
-//     await Post.findByIdAndUpdate()
-// })
+router.post("/:post/edit", async (req, res) =>{
+    const newText = req.body.postText
+    await Post.findByIdAndUpdate(req.params.post, {postText: newText});
+    res.redirect(`/${req.session.currentUser.username}`);
+})
 
 module.exports = router;
