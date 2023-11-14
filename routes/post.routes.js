bf0948332a43c5723cb7f3a7ad523f97f1756c1e
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-11-14 13:35:00 +0100

    delete in progress

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -8,16 +8,26 @@ const Post = require("../models/Post.model");
 
 //render view
 router.get("/:post/edit", async (req, res) =>{
-    // console.log("requparams",req.params);
+    console.log("requparams",req.params.post);
     selectedPost = await Post.findById(req.params.post)
+    console.log(selectedPost)
     res.render("post/editing-post", selectedPost);
     // console.log("post",selectedPost);
 })
 
 //redirect
 router.post("/:post/edit", async (req, res) =>{
+    // console.log(req.body.postText)
     const newText = req.body.postText
     await Post.findByIdAndUpdate(req.params.post, {postText: newText});
+    res.redirect(`/${req.session.currentUser.username}`);
+})
+
+router.get("/:post/delete", async (req, res) =>{
+    
+    
+    const idPost = req.params.post;
+    await Post.deleteOne({_id : idPost})
     res.redirect(`/${req.session.currentUser.username}`);
 })
 
