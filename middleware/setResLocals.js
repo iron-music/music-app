069fffd670df9959a363cdd0b8b2dba42358f369
Martin Palkov <martin.palkov@gmail.com>
2023-11-15 21:59:26 +0100

    added playback of song of the day on navbar

--- middleware/setResLocals.js
+++ middleware/setResLocals.js
@@ -1,10 +1,26 @@
-module.exports = (req, res, next) => {
+const Post = require("../models/Post.model");
+const WinnerSong = require("../models/WinnerSong.model");
+
+module.exports = async (req, res, next) => {
     //console.log("MIDDLEWARE. currentUser: ", req);
     if (req && req.session && req.session.currentUser) {
         res.locals.userLocals = req.session.currentUser;
     }
     else
         res.locals.userLocals = "";
+
+    // get Songoftheday info
+    const today = new Date();
+    today.setUTCHours(0,0,0,0);
+    const song = await WinnerSong.findOne({createdAt: {"$gt": today}}).populate("postId");
+    if (song) {
+        res.locals.winnerSong = song.postId;
+    }
+    else {
+        res.locals.winnerSong = "";
+    }
+
+    //console.log(song.postId);    
     next();
 };
 
