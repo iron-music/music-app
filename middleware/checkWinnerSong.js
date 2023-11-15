f238fc1cc331fea15ba23b2642f5806a79304f31
Martin Palkov <martin.palkov@gmail.com>
2023-11-15 19:35:57 +0100

    checkWinnerSong middleware working - commented

--- middleware/checkWinnerSong.js
+++ middleware/checkWinnerSong.js
@@ -5,6 +5,7 @@ const Post = require("../models/Post.model");
 module.exports = async (req, res, next) => {
     // console.log("MIDDLEWARE. currentUser: ", req);
     // check for winning song in the database
+    console.log("HOLA");
     const today = new Date();
     today.setUTCHours(0,0,0,0);
     
@@ -14,21 +15,29 @@ module.exports = async (req, res, next) => {
         "postId": "6554b71cbb358ef4f2dc739d"}); */
 
     let list = null;
-    const winnerSongOfToday = await WinnerSong.findOne({createdAt: {"$gt": today}});
+    let bestScoreSong;
+    let winnerSongOfToday = await WinnerSong.findOne({createdAt: {"$gt": today}});
     if (!winnerSongOfToday) {
         // search for songs of yesterday and find the greatest score among them.
         const yesterday = new Date (today);
         yesterday.setDate(yesterday.getDate() - 1);
 
-        list = await Post.find({"createdAt" : {"$gte": yesterday, "$lte":today}});
+        list = await Post.find({"createdAt" : {"$gte": yesterday, "$lte":today}}).sort({"score":-1});
+        bestScoreSong = list[0];
+
+        WinnerSong.create({postId: bestScoreSong._id});
+        console.log("OK WINNERSONG ADDED");
+
 
         // console.log("Today: ", today);
         // console.log("yesterday: ", yesterday);
         // if found, create WinnerSong in the Playlist collection
     }
+    else {
+        console.log("TODAY THE WINNER ALREADY EXISTS");
+    }
     // console.log("newDate: ", new Date('2023-11-15'), "\n Date: ", Date('2023-11-15'));
-    console.log("list: ", list);
-    return res.send(list);
+    return res.send(bestScoreSong);
     next();
 };
 
