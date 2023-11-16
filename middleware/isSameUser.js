c636ca08b230a7b34c50613bfc517450ae500d1c
Marc.A <marcalberopascual@gmail.com>
2023-11-16 16:32:54 +0100

    middlewares added

--- middleware/isSameUser.js
+++ middleware/isSameUser.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   
     // checks if the user is logged in when trying to access a specific page
+    console.log("midleware outside",req.session.currentUser.username,    req.params.user)
     if ( req.session.currentUser.username !== req.params.user) {
       return res.render("not-found");
     }
--- routes/postSong.routes.js
+++ routes/postSong.routes.js
@@ -5,10 +5,11 @@ const router = express.Router();
 const User = require("../models/User.model");
 const Post = require("../models/Post.model");
 const hasAlreadyVoted = require('../middleware/hasAlreadyVoted');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 
 //render view
-router.get("/:post/edit", async (req, res) =>{
+router.get("/:post/edit", isLoggedIn, async (req, res) =>{ 
     console.log("requparams",req.params.post);
     selectedPost = await Post.findById(req.params.post)
     console.log(selectedPost)
@@ -17,7 +18,7 @@ router.get("/:post/edit", async (req, res) =>{
 })
 
 //redirect
-router.post("/:post/edit", async (req, res) =>{
+router.post("/:post/edit", isLoggedIn, async (req, res) =>{
     // console.log(req.body.postText)
     const newText = req.body.postText
     const postId = req.params.post
@@ -25,14 +26,14 @@ router.post("/:post/edit", async (req, res) =>{
     res.redirect(`/${req.session.currentUser.username}`);
 })
 
-router.get("/:post/deleting", async (req, res) =>{
+router.get("/:post/deleting", isLoggedIn, async (req, res) =>{
     const postId = req.params.post;
     postInfo = await Post.find({_id : postId}).populate("owner");
     const objectPost = postInfo[0]
     res.render("post/deleting-confirmation-post",objectPost);
 })
 
-router.get("/:post/deleted", async (req, res) =>{
+router.get("/:post/deleted",isLoggedIn, async (req, res) =>{
     const idPost = req.params.post;
     const postPopulated = await Post.findById(idPost).populate("owner");
     const ownerId = postPopulated.owner._id;
@@ -42,7 +43,7 @@ router.get("/:post/deleted", async (req, res) =>{
 })
 
 //receiving the rating score and updating the Post score field
-router.post("/:post/rate", hasAlreadyVoted, async (req, res) => {
+router.post("/:post/rate", isLoggedIn, hasAlreadyVoted, async (req, res) => {
     const postId = req.params.post;
     const userId = req.session.currentUser._id;
     const rating = parseInt(req.body.rating);
